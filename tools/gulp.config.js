986fe1392a3d22ad0c7a8ecaee96ab52864a45ca
Kelly Domico <kelly.domico@hp.com>
2016-01-23 15:40:11 -0800

    Added Gulp tasks

--- tools/gulp.config.js
+++ tools/gulp.config.js
@@ -1,16 +1,32 @@
+'use strict';
+
 module.exports = function () {
   var paths = {
     src: '../src/',
     dist: '../dist/'
   };
+  
+  var srcLib = paths.src + 'lib/';
 
   var config = {
+    copyFiles: [
+      paths.src + '**/*',
+      '!' + srcLib + 'jquery{,/**}',  
+      '!' + srcLib + 'bootstrap-sass/assets/javascripts{,/**}',  
+    ],
+    
+    index: paths.src + 'index.html', 
+    
     paths: paths,
     
     wiredep: {
       bowerJson: require('./bower.json'),
-      directory: '../src/lib/',
-      ignorePath: '../src/'
+      directory: srcLib,
+      exclude: [
+        srcLib + 'jquery',
+        srcLib + 'bootstrap-sass/assets/javascripts'
+      ],
+      ignorePath: paths.src
     },
   }
   
--- tools/gulpfile.js
+++ tools/gulpfile.js
@@ -3,5 +3,30 @@
 var config = require('./gulp.config')(),
   del = require('del'),
   gulp = require('gulp'),
+  gutil = require('gulp-util'),
   sass = require('gulp-sass'),
   wiredep = require('wiredep').stream;
+  
+gulp.task('build:copy', ['clean'], function () {
+  return gulp
+    .src(config.copyFiles)
+    .pipe(gulp.dest(config.paths.dist));
+});
+
+gulp.task('build:inject', ['build:copy'], function () {
+  return gulp.src(config.index)
+    .pipe(wiredep(config.wiredep))
+    .pipe(gulp.dest(config.paths.dist));
+});
+
+gulp.task('build', ['build:inject'], function () {
+  gutil.log('Building...');  
+});
+
+gulp.task('clean', function () {
+  return del(config.paths.dist + '**/*', { force: true });
+});
+
+gulp.task('default', ['build'], function () {
+  gutil.log('Finished cleaning and building dist');
+});
